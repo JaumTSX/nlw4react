4dda4741a5d5aa9c55535f043bb3bd5c87e003b5
João Manuel <65908658+JaumTSX@users.noreply.github.com>
2021-02-24 12:36:44 -0300

    v1.3

---
+++ src/contexts/ChallengesContext.tsx
@@ -0,0 +1,71 @@
+import { createContext, useState, ReactNode } from 'react';
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number;
+}
+
+interface ChallengesContextData {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+  activeChallenge: Challenge;
+  experienceToNextLevel: number;
+  resetChallenge: () => void;
+  levelUp: () => void;
+  startNewChallenge: () => void;
+  completeChallenge: () => void;
+}
+
+interface ChallengesProviderProps {
+  children: ReactNode;
+}
+
+export const ChallengesContext = createContext({} as ChallengesContextData);
+
+export function ChallengesProvider({ children }: ChallengesProviderProps) {
+  const [level, setLevel] = useState(1);
+  const [currentExperience, setCurrentExperience] = useState(0);
+  const [challengesCompleted, setChallengesCompleted] = useState(0);
+
+  const [activeChallenge, setActiveChallenge] = useState(null);
+
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
+
+  function levelUp() {
+    setLevel(level + 1);
+  }
+
+  function startNewChallenge() {
+    const randomChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[randomChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
+  }
+
+  function completeChallenge() {
+    console.log('Completou');
+  }
+
+  return (
+    <ChallengesContext.Provider value={{ 
+      level, 
+      currentExperience, 
+      challengesCompleted, 
+      levelUp,
+      startNewChallenge,
+      activeChallenge,
+      resetChallenge,
+      completeChallenge,
+      experienceToNextLevel
+    }}>
+      {children}
+    </ChallengesContext.Provider>
+  );
+}
